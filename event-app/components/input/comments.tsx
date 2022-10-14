e0c3a220e6e2090c8e238b8ffa76e751165d7bf6
Leejin-Yang <ejin1996@gmail.com>
2022-10-14 15:09:59 +0900

    feat: 댓글 등록 알림 개인 작업

--- event-app/components/input/comments.tsx
+++ event-app/components/input/comments.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CommentList from './commentList'
 import NewComment from './newComment'
 import classes from './comments.module.css'
+import { NotificationContext } from '../../store/notificationContext'
 
 interface Props {
   eventId: string
@@ -11,6 +12,8 @@ interface Props {
 function Comments({ eventId }: Props) {
   const [showComments, setShowComments] = useState(false)
   const [comments, setComments] = useState<any[]>([])
+
+  const { showNotification } = useContext(NotificationContext)
 
   useEffect(() => {
     if (!showComments) return
@@ -26,14 +29,40 @@ function Comments({ eventId }: Props) {
     setShowComments((prevStatus) => !prevStatus)
   }
 
-  function addCommentHandler(commentData: any) {
-    fetch(`/api/comments/${eventId}`, {
-      method: 'POST',
-      body: JSON.stringify(commentData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  async function addCommentHandler(commentData: any) {
+    showNotification({
+      title: 'Sending Comment',
+      message: 'Post your comment',
+      status: 'pending',
     })
+
+    try {
+      const response = await fetch(`/api/comments/${eventId}`, {
+        method: 'POST',
+        body: JSON.stringify(commentData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await response.json()
+
+      if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong')
+      }
+
+      showNotification({
+        title: 'Success!',
+        message: data.message,
+        status: 'success',
+      })
+      setComments((prev) => [data.comments, ...prev])
+    } catch (error: any) {
+      showNotification({
+        title: 'Error!',
+        message: error.message || 'Something went wrong!',
+        status: 'error',
+      })
+    }
   }
 
   return (
