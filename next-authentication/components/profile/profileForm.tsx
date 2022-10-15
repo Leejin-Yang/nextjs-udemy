e95d5be03c82f67649cf85f85f84563fd3c58680
Leejin-Yang <ejin1996@gmail.com>
2022-10-15 18:30:02 +0900

    feat: 비밀번호 변경

--- next-authentication/components/profile/profileForm.tsx
+++ next-authentication/components/profile/profileForm.tsx
@@ -1,15 +1,42 @@
+import { signOut } from 'next-auth/react'
+import { FormEventHandler, useRef } from 'react'
 import styles from './profileForm.module.css'
+import type { PasswordData } from './userProfile'
 
-function ProfileForm() {
+interface Props {
+  changePassword: ({ oldPassword, newPassword }: PasswordData) => void
+}
+
+function ProfileForm({ changePassword }: Props) {
+  const newPasswordInputRef = useRef<HTMLInputElement>(null)
+  const oldPasswordInputRef = useRef<HTMLInputElement>(null)
+
+  const onPasswordChangeSubmit: FormEventHandler = (e) => {
+    e.preventDefault()
+
+    const enteredOldPassword = oldPasswordInputRef.current?.value
+    const enteredNewPassword = newPasswordInputRef.current?.value
+
+    // validation
+    if (!enteredOldPassword || !enteredNewPassword) return
+
+    changePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    })
+
+    signOut()
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={onPasswordChangeSubmit}>
       <div className={styles.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password' ref={oldPasswordInputRef} />
       </div>
       <div className={styles.action}>
         <button>Change Password</button>
--- next-authentication/components/profile/userProfile.tsx
+++ next-authentication/components/profile/userProfile.tsx
@@ -1,13 +1,37 @@
 import ProfileForm from './profileForm'
 import styles from './userProfile.module.css'
 
+export interface PasswordData {
+  oldPassword: string
+  newPassword: string
+}
+
 function UserProfile() {
-  // Redirect away if NOT auth
+  const changePassword = async (passwordData: PasswordData) => {
+    try {
+      const response = await fetch('/api/user/change-password', {
+        method: 'PATCH',
+        body: JSON.stringify(passwordData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await response.json()
+
+      if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong')
+      }
+
+      console.log(data)
+    } catch (error: any) {
+      console.log(error.message)
+    }
+  }
 
   return (
     <section className={styles.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm changePassword={changePassword} />
     </section>
   )
 }
